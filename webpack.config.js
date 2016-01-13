66e798d1d0f6947d7145297528fcf57a6eb1bbc9
jasondawson <jason.c.dawson@gmail.com>
2016-01-12 21:39:20 -0700

    config files

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+    devtool: 'eval',
+    context: __dirname + '/src',
+    entry: {
+        javascript: './index.js',
+    },
+    output: {
+        filename: 'bundle.js',
+        path: __dirname + '/build'
+    },
+    module: {
+        loaders: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+            query: {
+                cacheDirectory: true,
+                presets: ['react', 'es2015']
+            }
+        }]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx', '.html']
+    },
+    // watch: true
+    
+}
