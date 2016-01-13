d65df2d66816df0041fff8648718cc5dbc19e5c0
jasondawson <jason.c.dawson@gmail.com>
2016-01-12 21:31:31 -0700

    folders in external file for readability and disallow text selection

--- src/App.jsx
+++ src/App.jsx
@@ -3,73 +3,7 @@ import ReactDOM from 'react-dom';
 
 import Folder from './Folder';
 
-const file1 = {
-    type: 'file',
-    name: 'test.js'
-}
-
-const file2 = {
-    type: 'file',
-    name: 'test2.js'
-}
-
-const file3 = {
-    type: 'file',
-    name: 'test3.js'
-}
-const file4 = {
-    type: 'file',
-    name: 'youve-reached-the-end.js'
-}
-const folder1 = {
-    id: 1,
-    type: 'folder',
-    name: 'stuff',
-    contents: [file2, file3, file4]
-}
-const folder2 = {
-    id: 2,
-    type: 'folder',
-    name: 'stuff',
-    contents: [file2, file3, folder1]
-}
-const folder3 = {
-    id: 3,
-    type: 'folder',
-    name: 'stuff',
-    contents: [file2, file3, folder2]
-}
-const folder4 = {
-    id: 4,
-    type: 'folder',
-    name: 'stuff',
-    contents: [file2, file3, folder3]
-}
-const folder7 = {
-    id: 7,
-    type: 'folder',
-    name: 'seven',
-    contents: [file2]
-}
-const folder5 = {
-    id: 5,
-    type: 'folder',
-    name: 'stuff',
-    contents: [file2, file3, folder4, folder7]
-}
-const folder6 = {
-    id: 6,
-    type: 'folder',
-    name: 'stuff',
-    contents: [file2, file3, folder5]
-}
-
-const folder = {
-    id: 0,
-    type: 'folder',
-    name: 'root',
-    contents: [folder6, file1, file2]
-}
+import folders from './sampleFolderStructure';
 
 const App = React.createClass({
     getInitialState: function() {
@@ -87,7 +21,7 @@ const App = React.createClass({
     },
     render: function () {
         return (
-            <Folder folderDetails={folder} handleToggle={this.handleToggle} isOpen={this.state.isOpen}/>
+            <Folder folderDetails={folders.folder} handleToggle={this.handleToggle} isOpen={this.state.isOpen}/>
         )
     }
 });
